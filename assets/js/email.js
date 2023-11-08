8c0aa9f37ff512e8e2aedfef52e1b678b7f0d842
militaru32 <marius.militaru20@gmail.com>
2023-11-08 21:17:51 +0200

    Fix the contact page email script

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 // the email function is from Code institute and adapted for my needs
-function sendMmail (contactForm) {
+function sendMmail(contactForm) {
     let submitBtn = document.getElementById("tip-btn");
     let startButton = document.getElementById("start-button");
     let confirmationParagraph = document.createElement("p");
