84d37e8f4412b8d53a4b8a44be1af4fe5407b0f2
militaru32 <marius.militaru20@gmail.com>
2023-11-08 18:50:01 +0200

    Create const variables to get game elements

---
+++ assets/js/email.js
@@ -0,0 +1,9 @@
+// variables to get game elements 
+const gridContainer = document.querySelector(".grid");
+const startButton = document.getElementById("start-button");
+const movesDisplay = document.getElementById("moves");
+const timerDisplay = document.getElementById("timer");
+const grid = document.getElementById("grid");
+const resetButton = document.getElementById("reset-button");
+
+
