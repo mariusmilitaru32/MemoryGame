af353d924d2ef2fd5382d5a35245828043cd0d22
militaru32 <marius.militaru20@gmail.com>
2023-11-08 19:00:14 +0200

    Add function to create the card grid

--- assets/js/email.js
+++ assets/js/email.js
@@ -13,3 +13,31 @@ let moves = 0;
 let startTime = 0;
 let timerInterval;
 let gameStarted = false;
+
+// function to generate random cards from symbols
+const generateCardValues = () => {
+    const symbols = ["🥔", "🍒", "🥑", "🌽", "🥕", "🍇", "🍉", "🍌"];
+    const values = [];
+
+    for (let i = 0; i < 8; i++) {
+        const symbol = symbols[i % symbols.length];
+        values.push(symbol, symbol);
+    }
+
+    values.sort(() => Math.random() - 0.5);
+    cardValues = values;
+};
+
+//function to create the game board grid on index page
+const createGrid = () => {
+    gridContainer.innerHTML = "";
+    for (let i = 0; i < 16; i++) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.dataset.index = i;
+        card.dataset.symbol = cardValues[i];
+        card.textContent = cardValues[i];
+        card.addEventListener("click", () => onCardClick(card));
+        gridContainer.appendChild(card);
+    }
+};
