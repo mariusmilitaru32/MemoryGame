ff156578c338142c888642f9e98b504f38698462
militaru32 <marius.militaru20@gmail.com>
2023-11-14 23:17:06 +0200

    Add 404 page and countdown js function

---
+++ assets/js/count.js
@@ -0,0 +1,19 @@
+function redirectToHomePage() {
+    
+    var seconds = 10;
+
+    // Display a countdown message before redirecting
+    var countdown = setInterval(function() {
+        document.getElementById('countdown').innerHTML = "Redirecting you to home page in " + seconds + " seconds...";
+        seconds--;
+
+        // When the countdown is over, redirect to the home page
+        if (seconds < 0) {
+            clearInterval(countdown);
+            window.location.href = 'index.html'; 
+        }
+    }, 1000);
+}
+
+// Call the function when the page loads
+window.onload = redirectToHomePage;
