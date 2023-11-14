8b151bd60326bf468649288894accba556d287be
militaru32 <marius.militaru20@gmail.com>
2023-11-14 23:07:18 +0200

    Remove count down function from contact page

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,3 @@
-// the code is from code institute and adapted for email message confirmation and countdown
 function sendMail(contactForm) {
     let submitBtn = document.getElementById("tip-btn");
     let startButton = document.getElementById("start-button");
@@ -19,21 +18,6 @@ function sendMail(contactForm) {
             console.log("SUCCESS", response);
             confirmationParagraph.textContent = "Email has been sent successfully! Thank you";
             confirmationParagraph.style.display = "block";
-
-            // Wait for 3 seconds before starting the countdown
-            setTimeout(function() {
-                // Set up a countdown timer to automatically redirect to the home page after 10 seconds
-                let countdown = 10;
-                let countdownTimer = setInterval(function() {
-                    if (countdown === 0) {
-                        clearInterval(countdownTimer);
-                        window.location.href = "./index.html";
-                    } else {
-                        confirmationParagraph.textContent = `Redirecting you to home page in ${countdown} seconds!`;
-                        countdown--;
-                    }
-                }, 1000);
-            }, 3000);  // 3 seconds delay before the countdown is starting
         },
         function(error) {
             console.log("FAILED", error);
